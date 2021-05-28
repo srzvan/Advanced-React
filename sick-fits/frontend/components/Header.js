f522817095509e6fdf74c37cc16c90443004c531
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2021-05-28 17:56:35 +0300

    logo & header styles

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,15 +1,56 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  z-index: 2;
+  background: red;
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    padding: 0.5rem 1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar,
+  .sub-bar {
+    border-bottom: solid var(--black, black);
+  }
+
+  .bar {
+    display: grid;
+    align-items: center;
+    border-bottom-width: 10px;
+    justify-content: space-between;
+    grid-template-columns: auto 1fr;
+  }
+
+  .sub-bar {
+    display: grid;
+    border-bottom-width: 1px;
+    grid-template-columns: 1fr auto;
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
-        Sick fits
-        <Nav/>
+        <Logo>
+          <Link href="/">Sick fits</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
-    </header>
+      <Nav />
+    </HeaderStyles>
   );
 }
