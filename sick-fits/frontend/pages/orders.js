1e09303a3460977b67cc153f17f5c4a4e340862a
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2021-05-28 10:25:49 +0300

    pages templates

---
+++ sick-fits/frontend/pages/orders.js
@@ -0,0 +1,7 @@
+export default function OrdersPage() {
+  return (
+    <div>
+      <h1>Sell page</h1>
+    </div>
+  );
+}
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,7 @@
+export default function SellPage() {
+  return (
+    <div>
+      <h1>Sell page</h1>
+    </div>
+  );
+}
